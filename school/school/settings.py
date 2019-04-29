40b74d16ffbd2ebcd140097d8224a9980a7c30c7
Muhammad Tahir <tahirs95@hotmail.com>
2019-04-29 13:13:05 +0500

    Update settings.py

--- school/school/settings.py
+++ school/school/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nuk3c9%_&m&wvo0ks=r%tnkz_i3x#wqrn(1qw_9!uc(=cxo#0l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
